02cc0b3b11a4783f34b586fa89fa962c562065d4
burry <burry439@gmail.com>
2020-02-12 13:54:18 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wazimo';
+
+  columnDefs = [
+    {headerName : "Id", field : "id",sortable : true, filter : true},
+    {headerName : "First Name", field : "first_name",sortable : true, filter : true},
+    {headerName : "Last Name", field : "last_name",sortable : true, filter : true},
+    {headerName : "Email", field : "email",sortable : true, filter : true},
+    {headerName : "Gender", field : "gender",sortable : true, filter : true},
+    {headerName : "Ip address", field : "ip_address",sortable : true, filter : true}
+  ]
+
+  rowData : any;
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(){
+    this.rowData = this.http.get("https://storage.googleapis.com/static.aoni.io/demo/user.json")
+  }
+  
 }
